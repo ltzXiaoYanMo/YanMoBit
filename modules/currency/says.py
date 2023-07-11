b483d32a1f5588375d8dae108fdd7b8d8e2c78b6
ltzXiaoYanMo <115706851+ltzXiaoYanMo@users.noreply.github.com>
2023-07-12 01:48:42 +0800

    复读

--- modules/currency/says.py
+++ modules/currency/says.py
@@ -11,22 +11,22 @@ from graia.saya.builtins.broadcast.schema import ListenerSchema
 import botfunc
 
 channel = Channel.current()
-channel.name("echo")
-channel.description("echo 第一个输出")
-channel.author("HanTools")
+channel.name("says")
+channel.description("照搬KHB的says（")
+channel.author("ltzXiaoYanMo")
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
-async def echo(app: Ariadne, group: Group, source: Source, message: MessageChain = DetectPrefix("/echo ")):
-    if not str(message).startswith('/echo'):
+async def echo(app: Ariadne, group: Group, source: Source, message: MessageChain = DetectPrefix("/says ")):
+    if not str(message).startswith('/says'):
         m: ActiveGroupMessage = await app.send_group_message(
             group,
             message,
         )
-        botfunc.r.hset('echo', source.id, m.source.id)
+        botfunc.r.hset('says', source.id, m.source.id)
 
 
 @channel.use(ListenerSchema(listening_events=[GroupRecallEvent]))
 async def echo(app: Ariadne, group: Group, event: GroupRecallEvent):
-    if botfunc.r.hexists("echo", event.message_id):
-        await app.recall_message(botfunc.r.hget("echo", event.message_id), group)
+    if botfunc.r.hexists("says", event.message_id):
+        await app.recall_message(botfunc.r.hget("says", event.message_id), group)
