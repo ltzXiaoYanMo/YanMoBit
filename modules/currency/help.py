c34340fe5b16af49a48beb21a4bcea2ec3b9af75
ltzXiaoYanMo <2315049216@qq.com>
2023-08-16 21:07:06 +0800

    哼哼哼，啊啊啊啊啊啊啊啊啊啊

--- modules/currency/help.py
+++ modules/currency/help.py
@@ -1,10 +1,9 @@
 # 项目地址：https://github.com/ltzXiaoYanMo/YanBot_KHB_Edition
-
 from graia.amnesia.message import MessageChain
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
-from graia.ariadne.message.parser.base import MatchContent
-from graia.ariadne.util.saya import listen, decorate
+from graia.ariadne.message.parser.base import MentionMe
+from graia.ariadne.util.saya import listen
 from graia.saya import Channel
 from graia.ariadne.message.element import Plain, At
 
@@ -13,11 +12,9 @@ channel.name("帮助！")
 channel.description("这Bot怎么用啊！")
 channel.author("ltzXiaoYanMo")
 
-@listen(GroupMessage)
-@decorate(MatchContent("YB帮助"))
+@listen(MentionMe)
 async def help(app: Ariadne, event: GroupMessage):
     await app.send_group_message(
-        event.group_id,
         MessageChain(MessageChain(
-                [At(event.supplicant), Plain(" 前往https://bot.ymbot.top查看教程")]))
-        )
+            [At(event.supplicant), Plain(" 前往https://bot.ymbot.top查看教程")]))
+    )
