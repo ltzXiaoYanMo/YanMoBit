4743803f92b340a37bc284d234dbe06f98aa29eb
ltzXiaoYanMo <2315049216@qq.com>
2023-08-16 20:27:46 +0800

    哼哼哼，啊啊啊啊啊啊啊啊啊啊

--- modules/currency/help.py
+++ modules/currency/help.py
@@ -4,7 +4,6 @@ from graia.amnesia.message import MessageChain
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.parser.base import MatchContent
-from graia.ariadne.model import Group
 from graia.ariadne.util.saya import listen, decorate
 from graia.saya import Channel
 from graia.ariadne.message.element import Plain, At
@@ -16,11 +15,9 @@ channel.author("ltzXiaoYanMo")
 
 @listen(GroupMessage)
 @decorate(MatchContent("YB帮助"))
-async def no_six(app: Ariadne, group: Group, event: GroupMessage):
-    print(event.target)
-    if event.target == globalvars.bot_qq and event.context_type == "group":
-        await app.send_group_message(
-            event.group_id,
-            MessageChain(MessageChain(
+async def help(app: Ariadne, event: GroupMessage):
+    await app.send_group_message(
+        event.group_id,
+        MessageChain(MessageChain(
                 [At(event.supplicant), Plain(" 前往https://bot.ymbot.top查看教程")]))
         )
