e1c432e2ca70cff19eb9f85347ed9b05e5eba7e2
ltzXiaoYanMo <2315049216@qq.com>
2023-09-14 21:37:50 +0800

    什么垃圾代码（

--- modules/NO_USE/ping.py
+++ modules/NO_USE/ping.py
@@ -15,4 +15,18 @@ channel.author("ltzXiaoYanMo")
 
 result=os.system(MessageChain)
 if result:
-    MessageChain(os.system)
+    MessageChain(os.system.ping(MessageChain))
+    result = os.system(f"ping {MessageChain}")
+
+# 检测群聊中的ping后面的IP/域名，直接使用os模块中的ping此域名/IP
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def ping(app: Ariadne, group: Group, message: MessageChain):
+    if message.has(DetectPrefix):
+        return
+    if message.has(MessageChain.At):
+        return
+    if message.has(MessageChain.Plain):
+        if message.has(MessageChain.Plain, lambda m: m.text.startswith("ping")):
+            return
+        if message.has(MessageChain.Plain, lambda m: m.text.startswith("Ping")):
+            return
