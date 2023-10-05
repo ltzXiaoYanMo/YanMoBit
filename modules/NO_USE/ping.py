e5c27882891838fc27437bd56815c9772973a940
ltzXiaoYanMo <xiajiaruimail@qq.com>
2023-10-05 12:48:59 +0800

    你说我这四周去哪了？呃我也不知道（

--- modules/NO_USE/ping.py
+++ modules/NO_USE/ping.py
@@ -13,12 +13,14 @@ channel.name("Ping")
 channel.description("PingPing你的")
 channel.author("ltzXiaoYanMo")
 
-result=os.system(MessageChain)
-
 # 检测群聊中的ping后面的IP/域名，直接使用os模块中的ping此域名/IP
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def ping(app: Ariadne, group: Group, message: MessageChain):
     if message.has(DetectPrefix):
-        if message.has(DetectPrefix("ping")):
-            result=os.system(MessageChain)
-            return
+        if message.has(DetectPrefix("ping ")):
+            MessageChain(message.create(at=group.id, message=os.system(MessageChain)))
+
+# 收到消息，回复ping结果
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def on_message(app: Ariadne, group: Group, message: MessageChain):
+    MessageChain(message.replace("ping"))
