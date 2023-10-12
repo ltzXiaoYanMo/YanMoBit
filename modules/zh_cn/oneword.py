92eb5aa823718e608d9f791534c1bf0b882d4a5d
ltzXiaoYanMo <xiajiaruimail@qq.com>
2023-10-12 20:21:45 +0800

    修复一言

--- modules/zh_cn/oneword.py
+++ modules/zh_cn/oneword.py
@@ -17,18 +17,19 @@ channel.author("ltzXiaoYanMo")
 hitokoto = botfunc.get_config('oneword')
 
 # 收到API，获取一言
-data = requests.get(hitokoto)
+oneword = requests.get(hitokoto)
+
 
 @channel.use(
     ListenerSchema(
         listening_events=[GroupMessage],
-        decorators=[MatchContent("!oneword")]
+        decorators=[MatchContent("YB一言")]
     )
 )
 async def oneword(app: Ariadne, group: Group):
     # 收到API，获取一言
-    data = requests.get(hitokoto)
+    oneword = requests.get(hitokoto)
     await app.send_message(
         group,
-        data.text
+        oneword.text
     )
